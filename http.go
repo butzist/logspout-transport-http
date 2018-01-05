74bf30646f03541e31905b6dd2603a88ba092368
Adam Szalkowski <adam.szalkowski@alpiq.com>
2018-01-05 17:41:49 +0100

    add basic auth

--- http.go
+++ http.go
@@ -28,6 +28,8 @@ type httpTransport int
 type httpConnection struct {
 	client *http.Client
 	url string
+	user string
+	pass string
 }
 
 func (c *httpConnection) Read(b [] byte) (n int, err error) {
@@ -35,7 +37,19 @@ func (c *httpConnection) Read(b [] byte) (n int, err error) {
 }
 
 func (c *httpConnection) Write(b [] byte) (n int, err error) {
-	res, err := c.client.Post(c.url, "application/json", bytes.NewReader(b))
+	req, err := http.NewRequest(http.MethodPost, c.url, bytes.NewReader(b))
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(c.user, c.pass)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+
 	io.Copy(ioutil.Discard, res.Body)
 	return len(b), res.Body.Close()
 }
@@ -66,6 +80,6 @@ func (c *httpConnection) SetWriteDeadline(t time.Time) error {
 
 func (t *httpTransport) Dial(addr string, options map[string]string) (net.Conn, error) {
 	client := &http.Client{ Transport: &http.Transport{ IdleConnTimeout: 1 * time.Hour }}
-	conn := &httpConnection { client, "https://" + addr + "/" }
+	conn := &httpConnection { client, "https://" + addr + "/", options["http.user"], options["http.pass"] }
 	return conn, nil
 }
