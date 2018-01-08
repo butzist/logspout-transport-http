923358b2d71d9b0e04889814a9f8ef4002ea5fa6
Adam Szalkowski <adam.szalkowski@alpiq.com>
2018-01-08 16:54:15 +0100

    Add TLS client authentication

--- http.go
+++ http.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gliderlabs/logspout/router"
 	"crypto/tls"
 	"log"
+	"fmt"
 )
 
 func init() {
@@ -83,28 +84,77 @@ func (c *httpConnection) SetWriteDeadline(t time.Time) error {
 
 func (t *httpTransport) Dial(addr string, options map[string]string) (net.Conn, error) {
 	client := getClient(options)
-	conn := &httpConnection{client, "https://" + addr + "/", options["http.user"], options["http.pass"]}
+	protocol, ok := options["http.proto"]
+	if !ok {
+		protocol = "https"
+	}
+
+	conn := &httpConnection{client, fmt.Sprintf("%s://%s/", protocol, addr), options["http.user"], options["http.pass"]}
 	return conn, nil
 }
 
 func getClient(options map[string]string) *http.Client {
-	if pemFile, ok := options["http.ca"]; ok {
-		caCert, err := ioutil.ReadFile(pemFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		caCertPool := x509.NewCertPool()
-		caCertPool.AppendCertsFromPEM(caCert)
-
-		tlsConfig := &tls.Config{
-			RootCAs: caCertPool,
-		}
-		tlsConfig.BuildNameToCertificate()
-		transport := &http.Transport{TLSClientConfig: tlsConfig, IdleConnTimeout: 1 * time.Hour}
-		return &http.Client{Transport: transport}
-	} else {
-		transport := &http.Transport{IdleConnTimeout: 1 * time.Hour}
-		return &http.Client{Transport: transport}
+	tlsConfig, err := getTLSConfig(options)
+	if err != nil {
+		log.Fatalf("Error during TLS handshake: %s\n", err.Error())
 	}
+
+	transport := &http.Transport{TLSClientConfig: tlsConfig, IdleConnTimeout: 1 * time.Hour}
+	return &http.Client{Transport: transport}
+}
+
+func getTLSClientCerts(options map[string]string) ([]tls.Certificate, error) {
+	certPath, certOk := options["http.cert"]
+	keyPath, keyOk := options["http.key"]
+	var certs []tls.Certificate
+
+	if !certOk && !keyOk {
+		return certs, nil
+	} else if !certOk && !keyOk {
+		fmt.Printf("Missing TLS client certificate or key")
+		return nil, errors.New("TLS client configuration error")
+	}
+
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return certs, err
+	}
+
+	return []tls.Certificate{cert}, nil
+}
+
+func getRootCAs(options map[string]string) (*x509.CertPool, error) {
+	caPath, ok := options["http.ca"]
+	if !ok {
+		return nil, nil
+	}
+
+	caPool := x509.NewCertPool()
+
+	pem, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		return nil, err
+	}
+
+	ok = caPool.AppendCertsFromPEM(pem)
+	if !ok {
+		return nil, errors.New("Error parsing CA pem: " + caPath)
+	}
+
+	return caPool, nil
+}
+
+func getTLSConfig(options map[string]string) (*tls.Config, error) {
+	certs, err := getTLSClientCerts(options)
+	if err != nil {
+		return nil, err
+	}
+
+	CAs, err := getRootCAs(options)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &tls.Config{Certificates: certs, RootCAs: CAs}
+	return conf, nil
 }
